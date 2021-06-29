4de6f5a348bb214756a9325bbc92fdb999d27a37
Milan Radinovic <milanradinovic@rocketmail.com>
2021-06-29 16:42:40 +0200

    Implement header/navigation

---
+++ script.js
@@ -0,0 +1,29 @@
+const burger = document.querySelector(".burger");
+const closeBtn = document.querySelector(".close-btn");
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+const blurBg = document.querySelector(".blur-bg");
+let navOn = false;
+
+burger.addEventListener("click", () => {
+  nav.style.display = "block";
+  blurBg.style.display = "block";
+  navOn = true;
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.style.display = "none";
+  blurBg.style.display = "none";
+  navOn = false;
+});
+
+addEventListener("scroll", () => {
+  if (navOn) return;
+  if (scrollY > 100) {
+    console.log("test");
+    header.style.boxShadow = "1px 17px 21px -6px rgba(0,0,0,0.17)";
+  } else {
+    console.log("test2");
+    header.style.boxShadow = "";
+  }
+});
