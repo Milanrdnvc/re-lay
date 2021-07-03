9102e51c9bc00de6c49b72ce3de6bd8253f2a118
Milan Radinovic <milanradinovic@rocketmail.com>
2021-07-03 15:11:29 +0200

    Delete remove box shadow when nav is on (mobile) and fix edge cases with blur bg

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const closeBtn = document.querySelector(".close-btn");
 const nav = document.querySelector("nav");
 const header = document.querySelector(".header");
 const blurBg = document.querySelector(".blur-bg");
-let navOn = false;
 
 burger.addEventListener("click", () => {
   nav.style.display = "block";
   blurBg.style.display = "block";
-  navOn = true;
 });
 
 closeBtn.addEventListener("click", () => {
   nav.style.display = "none";
   blurBg.style.display = "none";
-  navOn = false;
+});
+
+addEventListener("resize", () => {
+  if (document.body.offsetWidth >= 950) {
+    nav.style.display = "block";
+    blurBg.style.display = "none";
+  } else {
+    nav.style.display = "none";
+  }
 });
 
 addEventListener("scroll", () => {
-  if (navOn) return;
   if (scrollY > 100) {
     header.style.boxShadow = "1px 17px 21px -6px rgba(0,0,0,0.17)";
   } else {
