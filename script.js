9326313926ed822fadec5db7765cacd99938d956
Milan Radinovic <milanradinovic@rocketmail.com>
2021-07-04 13:20:00 +0200

    Refactor parallax code and add comments

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const nav = document.querySelector("nav");
 const header = document.querySelector(".header");
 const blurBg = document.querySelector(".blur-bg");
 
-addEventListener("mousemove", (e) => {
-  document.querySelectorAll("[data-speed]").forEach((layer) => {
+function parallax(e) {
+  if (document.body.offsetWidth < 950) return;
+  document.querySelectorAll("[data-speed]").forEach(layer => {
     const speed = layer.getAttribute("data-speed");
     const x = (window.innerWidth - e.pageX * speed) / 1000;
     const y = (window.innerHeight - e.pageY * speed) / 1000;
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
-});
+}
 
+// toggle navbar
 burger.addEventListener("click", () => {
   nav.style.display = "block";
   blurBg.style.display = "block";
@@ -23,6 +25,22 @@ closeBtn.addEventListener("click", () => {
   blurBg.style.display = "none";
 });
 
+// add parallax effect
+addEventListener("mousemove", parallax);
+
+// remove parallax effect for smaller devices
+addEventListener("resize", () => {
+  if (document.body.offsetWidth < 950) {
+    removeEventListener("mousemove", parallax);
+    document.querySelectorAll("[data-speed]").forEach(layer => {
+      layer.style.transform = "";
+    });
+  } else {
+    addEventListener("mousemove", parallax);
+  }
+});
+
+// display navbar normally when on bigger screens and remove blurred background; else don't display it
 addEventListener("resize", () => {
   if (document.body.offsetWidth >= 950) {
     nav.style.display = "block";
@@ -32,6 +50,7 @@ addEventListener("resize", () => {
   }
 });
 
+// add shadow to the bottom of the header if we scroll
 addEventListener("scroll", () => {
   if (scrollY > 100) {
     header.style.boxShadow = "1px 17px 21px -6px rgba(0,0,0,0.17)";
